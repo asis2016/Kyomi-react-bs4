ed300085a81cddd54c9531fe9863787af1323c91
Ashish <mhrj.asis@gmail.com>
2021-05-19 14:39:26 +0200

    About update

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -7,9 +7,19 @@ import { AboutBase } from './About.style'
 
 const AboutData = [
 	{
+		primaryImage: 'about-1.png',
+		secondaryImage: 'about-1-bw.png',
+		name: 'Jane Doe',
+	},
+	{
 		primaryImage: 'about-2.png',
 		secondaryImage: 'about-2-bw.png',
-		title: 'Jane Doe',
+		name: 'Jane Doe',
+	},
+	{
+		primaryImage: 'about-3.jpg',
+		secondaryImage: 'about-3-bw.jpg',
+		name: 'Jane Doe',
 	},
 ]
 
@@ -37,19 +47,24 @@ const About = () => {
 
 			<Container>
 				<Row>
-					<Col lg={4} className={'text-center'}>
-						<ImageToggleOnMouseOver
-							primaryImage='https://raw.githubusercontent.com/asis2016/kyomi-react-bootstrap4/main/src/assets/images/about-1.png'
-							secondaryImage='https://raw.githubusercontent.com/asis2016/kyomi-react-bootstrap4/main/src/assets/images/about-2.png'
-						/>
-						<h2 className={'pt-2'}>Heading</h2>
-						<p>
-							Donec sed odio dui. Etiam porta sem malesuada magna mollis
-							euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.
-							Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-							Praesent commodo cursus magna.
-						</p>
-					</Col>
+					{AboutData.map((employee, index) => (
+						<Col lg={4} className={'text-center'} key={index}>
+							<ImageToggleOnMouseOver
+								primaryImage={
+									'https://raw.githubusercontent.com/asis2016/kyomi-react-bootstrap4/main/src/assets/images/' +
+									employee.primaryImage
+								}
+								secondaryImage='https://raw.githubusercontent.com/asis2016/kyomi-react-bootstrap4/main/src/assets/images/about-2.png'
+							/>
+							<h2 className={'pt-2'}>{employee.name}</h2>
+							<p>
+								Donec sed odio dui. Etiam porta sem malesuada magna mollis
+								euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.
+								Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+								Praesent commodo cursus magna.
+							</p>
+						</Col>
+					))}
 				</Row>
 			</Container>
 		</AboutBase>
