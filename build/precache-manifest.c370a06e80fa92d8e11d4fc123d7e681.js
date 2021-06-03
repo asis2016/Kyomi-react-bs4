a68cb4476689300cfbe1c0c4dccd6db543707b85
Ashish <mhrj.asis@gmail.com>
2021-06-03 09:54:52 +0200

    build

---
+++ build/precache-manifest.c370a06e80fa92d8e11d4fc123d7e681.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70565c7843f2e916cc5a41f2ba4f9a69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d2f32558962a5572a7a",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "a6fd40ddae6a59796884",
+    "url": "/static/css/main.96cda552.chunk.css"
+  },
+  {
+    "revision": "0d2f32558962a5572a7a",
+    "url": "/static/js/2.89b340f8.chunk.js"
+  },
+  {
+    "revision": "a6fd40ddae6a59796884",
+    "url": "/static/js/main.9c510735.chunk.js"
+  },
+  {
+    "revision": "9d0413ae8a9bbb75b4b1",
+    "url": "/static/js/runtime-main.322b4d90.js"
+  },
+  {
+    "revision": "f2b0d70c9565561a388796d7c4abf6e9",
+    "url": "/static/media/footer-bg.f2b0d70c.png"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "c2cfe8e2f9f5afbdd5f960f476d9e002",
+    "url": "/static/media/login-bg-2.c2cfe8e2.jpg"
+  },
+  {
+    "revision": "d95a6b11bcc4f6790702e8dbec92d0c7",
+    "url": "/static/media/phone.d95a6b11.svg"
+  },
+  {
+    "revision": "a1a78cc5098ece1c9fb67ce83c6710fe",
+    "url": "/static/media/screen-content-phone.a1a78cc5.jpg"
+  }
+]);
